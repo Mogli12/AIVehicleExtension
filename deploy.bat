50d3486f5adfd68a4083664104961381aab5b257
Mogli12 <mbiedens@gmail.com>
2021-04-11 12:27:12 +0200

    Translation RU

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,2 @@
-setlocal
-@set modname=FS19_AIVehicleExtension
-@xcopy %modname%.zip "C:\Users\d027464\Documents\My Games\FarmingSimulator2019\mods" /R/U/Y/Q
+@echo "mods"
+@xcopy FS19_AIVehicleExtension.zip "C:\Users\d027464\OneDrive\Dokumente\My Games\FarmingSimulator2019\mods" /R/U/Y/Q
